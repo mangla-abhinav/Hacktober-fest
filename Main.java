e921042895d3da34d6b43be8de33d45699fddd92
Rishi Datta <62599930+rishidatta8@users.noreply.github.com>
2020-10-16 12:45:20 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	float minFloat = Float.MIN_NORMAL;
+	float maxFloat = Float.MAX_VALUE;
+        System.out.println(minFloat+" "+maxFloat);
+        double minDouble = Double.MIN_NORMAL;
+        double maxDouble = Double.MAX_VALUE;
+        System.out.println(minDouble+" "+maxDouble);
+    }
+}
